a484768d4d51f6cdc1966f5d9a2498ed4a13eb57
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-10-28 18:28:03 -0300

    feat: initial homeScreen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,59 @@
+import { Box, Button, Grid } from "@mui/material";
+import { Container } from "@mui/system";
 import React from "react";
 
 import Cards from "../../components/Cards";
 import TopBar from "../../components/TopBar";
+import SearchTextField from "../../components/SearchTextField";
 
 export default function Home() {
   return (
     <>
       <TopBar />
-      <Cards />
+      <Box>
+        <SearchTextField placeholder="Digite aqui sua busca" />
+      </Box>
+      <Box>
+        <h2>Resultado de Busca</h2>
+        <Button>NovoCard</Button>
+      </Box>
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+          <Grid item xs={3}>
+            <Cards />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
