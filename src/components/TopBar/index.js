01d689262520b47d446f68687e73c09ca46c9ba5
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-10-28 18:27:51 -0300

    style: marginBottom on TopBar

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 
 export default function TopBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, marginBottom: "5em" }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
