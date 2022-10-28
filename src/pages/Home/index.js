f6dcbd4930592e9416344c122d326b54ed7c475b
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-10-28 18:00:32 -0300

    feat: add cards on HomeScreen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+
+import Cards from "../../components/Cards";
 import TopBar from "../../components/TopBar";
 
 export default function Home() {
   return (
     <>
       <TopBar />
+      <Cards />
     </>
   );
 }
