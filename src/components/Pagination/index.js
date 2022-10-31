bdd6d9623d89f3d9c8761c15a12ee3a17b885a58
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-10-31 00:26:55 -0300

    feat: pagination component

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 
-export default function PaginationComponent() {
-  return (
-    <Stack spacing={2}>
-      <Pagination count={10} color="secondary" />
-    </Stack>
-  );
+export default function PaginationComponent({ onClick }) {
+  return <Pagination count={10} color="secondary" onClick={onClick} />;
 }
