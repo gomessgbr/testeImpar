8c114504df2c02d739062c87a66381d76fa96e71
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-01 13:35:49 -0300

    feat: change name for alertModal

--- src/components/AlertModal/index.jsx
+++ src/components/AlertModal/index.jsx
@@ -5,7 +5,7 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
-export default function AlertDialog({ showModal, handleClose }) {
+export default function AlertModal({ showModal, handleClose }) {
   return (
     <>
       <Dialog
