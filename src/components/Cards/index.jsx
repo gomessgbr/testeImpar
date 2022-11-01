221432a4967c76be1bbdd9eeec02db9d69aa21bb
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-01 09:15:34 -0300

    style: border on bottom card box

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -9,32 +9,33 @@ import Stack from "@mui/material/Stack";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import Typography from "@mui/material/Typography";
+import { Box } from "@mui/material";
 
 export default function Cards({ namePokemons, image }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="240"
-        image={image}
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {namePokemons}
-        </Typography>
-      </CardContent>
+      <Box borderBottom={0.5} borderColor="#a0a0a0">
+        <CardMedia component="img" image={image} alt="green iguana" />
+        <CardContent>
+          <Typography flex={1}>{namePokemons}</Typography>
+        </CardContent>
+      </Box>
       <CardActions>
         <Stack
           direction="row"
           alignItems="center"
           justifyContent="space-around"
           flex={1}
-          borderTop={0.5}
-          borderColor="#a0a0a0"
         >
-          <Button startIcon={<DeleteOutlineOutlinedIcon />}>Excluir</Button>
-          <Button startIcon={<ModeEditOutlineOutlinedIcon />}>Editar</Button>
+          <Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}>
+            Excluir
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<ModeEditOutlineOutlinedIcon />}
+          >
+            Editar
+          </Button>
         </Stack>
       </CardActions>
     </Card>
