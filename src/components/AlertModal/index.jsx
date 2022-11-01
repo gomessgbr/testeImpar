f81abbac87f8c239d4b3438171ac8198e8b60632
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-01 13:34:38 -0300

    feat: create a AlertComponent

---
+++ src/components/AlertModal/index.jsx
@@ -0,0 +1,29 @@
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function AlertDialog({ showModal, handleClose }) {
+  return (
+    <>
+      <Dialog
+        open={showModal}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Alerta</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Funcionalidade não implementada
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
